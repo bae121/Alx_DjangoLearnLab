87d49a9c18b91df45dba9327dd8059486f2607b4
bae121 <akpablielikembright999@gmail.com>
2025-11-08 18:12:11 +0000

    django-models

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,15 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author (Kofi Akpabli)
+akpabli = Author.objects.get(name="Kofi Akpabli")
+books_by_akpabli = Book.objects.filter(author=akpabli)
+print("Books by Kofi Akpabli:", [book.title for book in books_by_akpabli])
+
+# List all books in a library
+central_library = Library.objects.get(name="Central Library")
+books_in_library = central_library.books.all()
+print("Books in Central Library:", [book.title for book in books_in_library])
+
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=central_library)
+print("Librarian of Central Library:", librarian.name)
