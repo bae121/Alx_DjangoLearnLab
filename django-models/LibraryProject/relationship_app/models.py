711090a841087333aa490f359ab873801bc772f9
bae121 <akpablielikembright999@gmail.com>
2025-11-09 19:41:04 +0000

    django-models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .models import Author
+from django.contrib.auth.decorators import permission_required
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
