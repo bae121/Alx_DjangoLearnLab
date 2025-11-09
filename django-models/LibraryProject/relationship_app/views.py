40d75e2d09934623ce6b367b526bf363eb6d5c3f
bae121 <akpablielikembright999@gmail.com>
2025-11-09 18:11:00 +0000

    django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,10 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
 
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
 def book_list_view(request):
    books = Book.objects.all()
    return render(request, "relationship_app/list_books.html", {"books": books})
@@ -18,3 +22,30 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["books"] = self.object.books.all()
         return context
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("book_list")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("book_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
