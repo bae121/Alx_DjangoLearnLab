393b56c2905f6a7cacedca2a7930b6d5e25e2186
bae121 <akpablielikembright999@gmail.com>
2025-11-09 19:26:29 +0000

    django-mdoels

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import admin_view, librarian_view, member_view
+from .views import admin_view, librarian_view, member_view, add_book, edit_book, delete_book
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,7 @@ urlpatterns = [
     path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
     path('member-role/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-
-# Create your models here.
+from .models import Author
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+        
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
