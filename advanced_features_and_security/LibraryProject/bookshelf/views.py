6e1a2d4fe314a6753976856cc34faed201dcc58d
bae121 <akpablielikembright999@gmail.com>
2025-11-15 01:34:10 +0000

    advanced_features_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Article
+from .models import Book
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def article_list(request):
