ad29e9b9bb59d2281cf5a6aee485b0a1f15c5cda
bae121 <akpablielikembright999@gmail.com>
2025-11-28 02:18:43 +0000

    advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+# ListView retrieves all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['publication_year', 'title']
+
+
+# DetailView retrieves a single book by ID
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+# CreateView adds a new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        """
+        This override is created to customize response format and ensure validation errors
+        are returned clearly.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(
+            {"message": "Book created successfully!", "book": serializer.data},
+            status=status.HTTP_201_CREATED
+        )
+
+# UpdateView modifys an existing book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        """
+        Override update to customize response and enforce validation.
+        """
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(
+            {"message": "Book updated successfully!", "book": serializer.data},
+            status=status.HTTP_200_OK
+        )
+
+# DeleteView removes a book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+    
